f9b74330f7b57ad2428ad4da6196c13541e0a836
RaphaelHardFork <raphael.pellet@protonmail.com>
2022-06-09 14:14:01 +0200

    abi export

---
+++ scripts/utils/abiList.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-process-exit */
+import { readFile, writeFile } from "fs/promises";
+import { Root } from "./deployment";
+import hre from "hardhat";
+
+// This file is then imported in the front-end
+const FILE_PATH = "./scripts/utils/contracts.json";
+
+type Contract = {
+  address: string;
+  abi: any[];
+};
+type ContractName = { [name: string]: Contract };
+type Chain = { [network: string]: ContractName };
+
+const createList = async () => {
+  let parsedJson: Root;
+  try {
+    parsedJson = JSON.parse(
+      await readFile("./scripts/utils/deployed.json", "utf-8")
+    );
+  } catch (e) {
+    console.log("Cannot read deployed.json or no deployed.json file");
+    process.exit(1);
+  }
+
+  // Write or update file
+  const _contract: Contract = {} as Contract;
+  const _contractName: ContractName = {};
+  const _chain: Chain = {};
+
+  for (const contract of parsedJson.list) {
+    _contract.address =
+      parsedJson.contracts[contract][hre.network.name].address;
+
+    const file: any = await readFile(`out/${contract}.sol/${contract}.json`);
+    const abi = JSON.parse(file).abi;
+    _contract.abi = abi;
+    _contractName[contract] = _contract;
+  }
+  _chain[hre.network.name] = _contractName;
+
+  const jsonString = JSON.stringify(_chain);
+
+  try {
+    await writeFile(FILE_PATH, jsonString);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+createList();
